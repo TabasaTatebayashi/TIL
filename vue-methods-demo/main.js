df6c2bd3222bd6434ff509adde06e04b6510e774
TabasaTatebayashi <tabasa.tatebayashi@rensa.co.jp>
2025-08-06 15:34:32 +0900

    first commit

---
+++ vue-methods-demo/main.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#app",
+    data: {
+        count: 0
+    },
+    methods: {
+        countUp: function(){
+            this.count++;
+        },
+        reset: function(){
+            this.count = 0;
+        }
+    }
+});
